620da0f4d47aa3ddb8d17c21515959a67db12754
fvz233 <fd02fmed@gmail.com>
2026-08-06 12:04:23 +0300

    Bump asset cache-busting version constant to 1.0.52

--- plugin.php
+++ plugin.php
@@ -60,7 +60,7 @@ function multi_sync_redact_debug_value($value, $key = '')
     return $value;
 }
 
-define('MULTI_SYNC_VERSION', '1.0.46');
+define('MULTI_SYNC_VERSION', '1.0.52');
 define('MULTI_SYNC_SCHEMA_VERSION', '20260802-2');
 define('MULTI_SYNC_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('MULTI_SYNC_PLUGIN_URL', plugin_dir_url(__FILE__));
